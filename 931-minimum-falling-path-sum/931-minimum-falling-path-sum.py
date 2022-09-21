9b210cb203d9adc7da30fa3532b23666a2555616
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-09-21 10:54:26 +0300

    Time: 510 ms (5.09%), Space: 14.8 MB (65.56%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,26 +1,19 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        inbound = lambda row,col : 0 <= row < len(matrix) and 0 <= col < len(matrix[0])
-        dirs = [[1,-1],[1,0],[1,1]]
-        visited = {}
-        
-        def dfs(row,col):
-            if row == len(matrix)-1:
-                return matrix[row][col]
-            if (row,col) in visited:
-                return visited[(row,col)]
-            min_ = float('inf')
-            for dir_ in dirs:
-                new_row,new_col = row + dir_[0],col + dir_[1]
-                if inbound(new_row,new_col):
-                    cur = dfs(new_row,new_col)
-                    min_ = min(min_,cur)
-            # print(matrix[row][col],min_)
-            visited[(row,col)] = matrix[row][col] + min_
-            return matrix[row][col] + min_
-        
-        
-        min_ = float('inf')
-        for i in range(len(matrix[0])):
-            min_ = min(min_,dfs(0,i))
-        return min_
+        # dp
+        dp = [[float('inf') for _ in matrix[0]] for _ in matrix]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i == 0:
+                    dp[0][j] = matrix[0][j]
+                else:
+                    for k in (-1,0,1):
+                        if j+k >= 0 and j+k < len(matrix[0]):
+                            # print(dp[i][j],dp[i-1][j+k],matrix[i][j])
+                            dp[i][j] = min(dp[i][j] , dp[i-1][j+k]+matrix[i][j])
+        ans = float('inf')
+        # print(dp)
+        for val in dp[-1]:
+            ans = min(ans,val)
+        return ans
+                
