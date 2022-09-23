be66f698ad5bdce23b0300db48d2a821d9e5ac90
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-09-23 11:17:05 +0300

    Time: 2687 ms (22.88%), Space: 32.9 MB (80.82%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.py
@@ -1,14 +1,16 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        pq = [(nums1[0] + nums2[0], 0, 0)]
         ans = []
-        heap = []
-        heapq.heapify(heap)
-        for i in range(len(nums1)):
-            heapq.heappush(heap,(nums1[i]+nums2[0],i,0))
-        while heap and k > 0:
-            sum_,i,j = heapq.heappop(heap)
-            ans.append([nums1[i],nums2[j]])
-            if j + 1 < len(nums2):
-                heapq.heappush(heap,(nums1[i]+nums2[j+1],i,j+1))
+        vis = set([(0, 0)])
+        while k and pq:
+            s, i1, i2 = heappop(pq)
+            ans.append([nums1[i1], nums2[i2]])
+            if i1 + 1 < len(nums1) and (i1 + 1, i2) not in vis:
+                heappush(pq, (nums1[i1 + 1] + nums2[i2], i1 + 1, i2))
+                vis.add((i1 + 1, i2))
+            if i2 + 1 < len(nums2) and (i1, i2 + 1) not in vis:
+                heappush(pq, (nums1[i1] + nums2[i2 + 1], i1, i2 + 1))
+                vis.add((i1, i2 + 1))
             k -= 1
         return ans
