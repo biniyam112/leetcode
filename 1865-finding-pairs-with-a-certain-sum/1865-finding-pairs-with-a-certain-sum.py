89bd53e67a6d02158b0ded09a41cb4c8d5233210
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-09-24 17:18:58 +0300

    Time: 3210 ms (5.06%), Space: 46.4 MB (35.80%) - LeetHub

--- 1865-finding-pairs-with-a-certain-sum/1865-finding-pairs-with-a-certain-sum.py
+++ 1865-finding-pairs-with-a-certain-sum/1865-finding-pairs-with-a-certain-sum.py
@@ -1,38 +1,21 @@
 class FindSumPairs:
 
     def __init__(self, nums1: List[int], nums2: List[int]):
-        self.nums1 = nums1
-        # self.nums1 = {}
-        # for n in nums1:
-        #     if n in self.nums1:
-        #         self.nums1[n] += 1
-        #     else:
-        #         self.nums1[n] = 1
-        self.nums2 = {}
-        self.sum = {}
-        for i,n in enumerate(nums2):
-            self.nums2[i] = n
-            if n in self.sum:
-                self.sum[n] += 1
-            else:
-                self.sum[n] = 1
+        self.nums1,self.nums2 = nums1,nums2
+        self.sum = Counter(nums2)
             
         
 
     def add(self, index: int, val: int) -> None:
         self.sum[self.nums2[index]] -= 1
         self.nums2[index] += val
-        if self.nums2[index] not in self.sum:
-            self.sum[self.nums2[index]] = 1
-        else:
-            self.sum[self.nums2[index]] += 1
+        self.sum[self.nums2[index]] += 1
         
 
     def count(self, tot: int) -> int:
         counter = 0
         for n in self.nums1:
-            if tot-n in self.sum:
-                counter += self.sum[tot-n]
+            counter += self.sum[tot-n]
         return counter
         
 
