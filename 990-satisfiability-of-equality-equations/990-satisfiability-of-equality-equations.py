ae5c5b4628ba0626abf35889d10fb320d8f33dcc
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-09-26 19:51:16 +0300

    Time: 90 ms (32.60%), Space: 14.2 MB (9.51%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -15,11 +15,8 @@ class Solution:
         for equation in equations:
             if equation[1] == '=':
                 union(equation[0],equation[3])
-        # print(parent)
         
         for equation in equations:
-            # print(equation[0],find(equation[0]))
-            # print(equation[3],find(equation[3]))
             if equation[1] == '=' and find(equation[0]) != find(equation[3]):
                 return False
             if equation[1] == '!' and find(equation[0]) == find(equation[3]):
