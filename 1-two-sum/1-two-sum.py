3ceb109aea19c58d09a238c2ac45a073e4106cff
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-09-28 18:52:27 +0300

    Time: 134 ms (45.65%), Space: 15.6 MB (8.83%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -4,20 +4,16 @@ class Solution:
     #      ij
     # add every element in set
     # iterate on each item and check if target - num is in the set
-        # i = 0
-        # j = len(nums)-1
-        # nums.sort()
-        # while i <= j:
-        #     sum_ = nums[i]+nums[j]
-        #     if sum_ == target:
-        #         return [i,j]
-        #     elif sum_ > target:
-        #         j -= 1
-        #     else:
-        #         i += 1
-        items = defaultdict(int)
-        for i,num in enumerate(nums):
-            items[num] = i
-        for i,num in enumerate(nums):
-            if target - num in items and i != items[target-num]:
-                return [items[target-num],i]
+        i = 0
+        j = len(nums)-1
+        for k,num in enumerate(nums):
+            nums[k] = (num,k)
+        nums.sort()
+        while i < j:
+            sum_ = nums[i][0] + nums[j][0]
+            if sum_ == target:
+                return [nums[i][1],nums[j][1]]
+            elif sum_ > target:
+                j -= 1
+            else:
+                i += 1
