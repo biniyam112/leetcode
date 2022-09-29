bb15a6d7aa2cf3a7618d13b0dcc75c8967e4ef71
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-09-29 16:05:53 +0300

    Time: 530 ms (50.29%), Space: 15.5 MB (80.93%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,26 +1,33 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        index = bisect_left(arr,x)
+        i,j = index,index
+        # print(index,x)
         ans = []
-        for i,num in enumerate(arr):
-            arr[i] = (abs(x-num),num)
-        heapq.heapify(arr)
-        while arr and k > 0:
-            item = heapq.heappop(arr)
-            ans.append(item[1])
+        if index < len(arr) and arr[index]-x == 0:
+            ans.append(arr[index])
+            i -= 1
+            j += 1
             k -= 1
+        else:
+            i -= 1
+        while i>=0 and j<len(arr) and k>0:
+            if abs(x-arr[i]) > abs(x-arr[j]):
+                ans.append(arr[j])
+                j+=1
+                k -= 1
+            else:
+                ans.append(arr[i])
+                i-=1
+                k -= 1
+        while k > 0 and j < len(arr):
+            ans.append(arr[j])
+            j += 1
+            k-=1
+        while k > 0 and i >= 0:
+            ans.append(arr[i])
+            i -= 1
+            k-=1
+            
         ans.sort()
         return ans
-    
-    
-    
-#         index = bisect_left(items,3)
-#         i,j = index,index
-#         if arr[index]-x == 0:
-#             ans += index
-#             k -= 1
-#             i -= 1
-#             j += 1
-#         while i<=0 and j<len(arr) and k>0:
-            
-        
-#         return ans
