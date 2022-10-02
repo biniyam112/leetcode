a1c5db21fda82bcb02991d451b2312b293eb5584
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-10-02 21:35:22 +0300

    Time: 2249 ms (6.23%), Space: 16.1 MB (48.59%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,6 +1,9 @@
 class Solution:
-    @cache
+    def __init__(self):
+        self.visited = {}
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        if (n,target) in self.visited:
+            return self.visited[(n,target)]
         if n == 0:
             if target == 0:
                 return 1
@@ -8,4 +11,5 @@ class Solution:
         ways = 0
         for i in range(1,k+1):
             ways += self.numRollsToTarget(n-1,k,target-i)
+        self.visited[(n,target)] = ways
         return ways % (10**9+7)
