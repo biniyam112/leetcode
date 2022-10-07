1ce849259a126446bdb58c0f45c35fadeade73db
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-10-07 17:51:23 +0300

    Time: 507 ms (90.15%), Space: 14.9 MB (32.00%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.py
+++ 732-my-calendar-iii/732-my-calendar-iii.py
@@ -1,22 +1,22 @@
-from sortedcontainers import SortedDict
+from sortedcontainers import SortedList
 
 class MyCalendarThree:
 
     def __init__(self):
-        self.diff = SortedDict()
-        
+        # only store the starting point and count of events
+        self.starts = SortedList([[0,0]])
+        self.res = 0
+
+    def split(self, x: int) -> None:
+        idx = self.starts.bisect_left([x,0])
+        if idx < len(self.starts) and self.starts[idx][0] == x:
+            return idx
+        self.starts.add([x,self.starts[idx-1][1]])
 
     def book(self, start: int, end: int) -> int:
-        self.diff[start] = self.diff.get(start, 0) + 1
-        self.diff[end] = self.diff.get(end, 0) - 1
-        cur = res = 0
-        for delta in self.diff.values():
-            cur += delta
-            res = max(cur, res)
-        return res
-        
-
-
-# Your MyCalendarThree object will be instantiated and called as such:
-# obj = MyCalendarThree()
-# param_1 = obj.book(start,end)
+        self.split(start)
+        self.split(end)
+        for interval in self.starts.irange([start,0], [end,0], (True,False)):
+            interval[1] += 1
+            self.res = max(self.res, interval[1])
+        return self.res
