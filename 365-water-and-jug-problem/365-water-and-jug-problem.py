bb2815d8acbe01f84a8a33f8e06f0c8806f90bd7
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-10-10 21:09:41 +0300

    Time: 6398 ms (11.80%), Space: 81.4 MB (14.80%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.py
+++ 365-water-and-jug-problem/365-water-and-jug-problem.py
@@ -1,5 +1,28 @@
 class Solution:
-    def canMeasureWater(self, jug1: int, jug2: int, tar: int) -> bool:
-        if(jug1 + jug2 < tar):
-            return 0
-        return tar % math.gcd(jug1, jug2) == 0
+    def canMeasureWater(self, jug1: int, jug2: int, target: int) -> bool:
+        queue = collections.deque()
+        queue.append((0,0))
+        visited = set()
+        visited.add((0,0))
+        
+        while queue:
+            cur1,cur2 = queue.popleft()
+            if cur1 + cur2 == target:
+                return True
+            options = []
+            # empty one
+            options.append((0,cur2))
+            options.append((cur1,cur2))
+            # fill one with another
+            fill1 = min(jug1-cur1,cur2)
+            options.append((cur1+fill1,cur2-fill1))
+            fill2 = min(jug2-cur2,cur1)
+            options.append((cur1-fill2,cur2+fill2))
+            # fill one with water
+            options.append((jug1,cur2))
+            options.append((cur1,jug2))
+            for option in options:
+                if option not in visited:
+                    visited.add(option)
+                    queue.append(option)
+        return False
