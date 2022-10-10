c17a9dd7df98b975d41d5fbab90a750b45348df4
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-10-10 12:25:52 +0300

    Time: 7245 ms (5.01%), Space: 16.5 MB (5.89%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.py
+++ 752-open-the-lock/752-open-the-lock.py
@@ -15,14 +15,13 @@ class Solution:
             comb,steps = queue.popleft()
             if comb == target:
                 return steps
-            if comb not in deadends:
-                for i in range(4):
-                    inc = ''.join([comb[j] if j != i else increase(comb[j]) for j in range(len(comb))])
-                    dec = ''.join([comb[j] if j != i else decrease(comb[j]) for j in range(len(comb))])
-                    if inc not in deadends:
-                        queue.append((inc,steps+1))
-                    if dec not in deadends:
-                        queue.append((dec,steps+1))
-                deadends.add(comb)
+            for i in range(4):
+                inc = ''.join([comb[j] if j != i else increase(comb[j]) for j in range(len(comb))])
+                dec = ''.join([comb[j] if j != i else decrease(comb[j]) for j in range(len(comb))])
+                if comb not in deadends:
+                    queue.append((inc,steps+1))
+                if comb not in deadends:
+                    queue.append((dec,steps+1))
+            deadends.add(comb)
         return -1
     
