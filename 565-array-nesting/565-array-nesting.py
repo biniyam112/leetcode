fa8873385d546c6063063b7505c2859bf0a750cc
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-10-10 15:02:37 +0300

    Time: 2174 ms (26.51%), Space: 32.3 MB (53.42%) - LeetHub

--- 565-array-nesting/565-array-nesting.py
+++ 565-array-nesting/565-array-nesting.py
@@ -1,5 +1,25 @@
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
+        # bfs
+        maxsteps = 0
+        visited = defaultdict(int)
+        for i in range(len(nums)):
+            queue = deque()
+            queue.append((i,0))
+            while queue:
+                val,steps = queue.popleft()
+                if val in visited:
+                    maxsteps = max(maxsteps,steps+visited[val])
+                    visited[val] = steps
+                    break
+                else:
+                    queue.append((nums[val],steps+1))
+                visited[val] = 0
+        return maxsteps
+                    
+        
+        
+        # dfs
         visited = defaultdict(int)
         def dfs(val):
             if val in visited:
@@ -12,4 +32,6 @@ class Solution:
         for i in range(len(nums)):
             ans = max(ans,dfs(i))
         return ans
+    
+
             
