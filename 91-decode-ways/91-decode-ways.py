f5d9dd1a9d998ece6eff8f9720f33aabc637a756
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-10-30 21:30:06 +0300

    Time: 69 ms (24.62%), Space: 14.3 MB (11.92%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,5 +1,6 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # dp recursive
         if s[0] == '0':
             return 0
         @cache
@@ -17,3 +18,18 @@ class Solution:
             one_step = recursion(index+1)
             return one_step+two_steps
         return recursion(0)
+
+        # dp iterative
+        if s[0]=="0":
+            return 0
+        dp=[1, 1]
+        prev=s[0]
+        for each in s[1:]:
+            result=0
+            if each!="0":
+                result+=dp[-1]
+            if prev!="0" and int(prev+each)<=26:
+                result+=dp[-2]
+            dp.append(result)
+            prev=each
+        return dp[-1]
