8251f5f9f95da59e43dbdf597bba15175116fd15
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-11-02 13:47:41 +0300

    Time: 1076 ms (83.58%), Space: 27.6 MB (49.29%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.py
+++ 848-shifting-letters/848-shifting-letters.py
@@ -1,8 +1,21 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
-        ans = []
+        ans = deque()
+        pre = 0
+        # little bit improvement in the time compexity
+        # do presum and shifting in on iteration
+        for i in range(len(shifts)-1,-1,-1):
+            shifts[i] += pre
+            pre = shifts[i]
+            cur = (shifts[i]+ord(s[i])-97)%26 + 97
+            ans.appendleft(chr(cur))
+        return ''.join(ans)
+            
+        # reverse presum
         for i in range(len(shifts)-1,0,-1):
             shifts[i-1] += shifts[i]
+            
+        # shift characters
         for i in range(len(shifts)):
             cur = (shifts[i]+ord(s[i])-97) % 26 + 97
             ans.append(chr(cur))
