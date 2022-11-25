9a29fe44213de8e0cac77e256984a37d54696489
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-11-25 16:33:54 +0300

    Time: 6944 ms (8.60%), Space: 85.8 MB (13.08%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.py
+++ 935-knight-dialer/935-knight-dialer.py
@@ -1,42 +1,26 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
         
-        moves = [[2,1],[2,-1],[-2,1],[-2,-1],[1,-2],[-1,-2],[1,2],[-1,2]]
-        valid = lambda row,col : 0 <= row < len(board) and 0 <= col <len(board[0]) and board[row][col] != None
         
-
-        board = [[[] for _ in range(3)] for _ in range(3)]
-        board.append([None,[],None])
+        keys = [[4, 6], [6, 8], [7, 9], [4, 8], [3, 9, 0], [],[1, 7, 0], [2, 6], [1, 3], [2, 4]]
         
         ans = 0
-        for i in range(4):
-            for j in range(3):
-                if board[i][j] != None:
-                    for x,y in moves:
-                        new_row,new_col = i+x,j+y
-                        if valid(new_row,new_col):
-                            board[i][j].append((new_row,new_col))
-        
-        def dfs(row,col,steps):
+        def dfs(key,steps):
             if steps == n:
                 return 1
             res = 0
-            if (row,col,steps) in visited:
-                return visited[(row,col,steps)]
-            for cell in board[row][col]:
-                res += dfs(cell[0],cell[1],steps+1)
-            visited[(row,col,steps)] = res
+            if (key,steps) in visited:
+                return visited[(key,steps)]
+            for next_key in keys[key]:
+                res += dfs(next_key,steps+1)
+            visited[(key,steps)] = res
             return res
         
-            
+        
         ans = 0
         visited = {}
-        for i in range(4):
-            for j in range(3):
-                if board[i][j] != None:
-                    ans += dfs(i,j,1)
-        
-        
+        for i in range(len(keys)):
+            ans += dfs(i,1)
         return ans % (10**9+7)
             
             
