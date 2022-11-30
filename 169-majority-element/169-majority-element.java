d333d68f99d1b6676075104a63aa69c7df5fe195
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-11-30 11:25:26 +0300

    Time: 20 ms (32.29%), Space: 57.1 MB (5.41%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -4,7 +4,7 @@ class Solution {
         int i = 0;
         for(int j = 0;j < nums.length; j++){
             if(nums[j] != nums[i]){
-                if(j-1+1 > nums.length/2){
+                if(j > nums.length/2){
                     return nums[i];
                 }
                 i = j;
