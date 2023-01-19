47461c9fe8bc6c8056617ffc56441f8137d03818
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-01-19 15:39:29 +0300

    Time: 451 ms (98.60%), Space: 18.8 MB (91.66%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -4,27 +4,30 @@ class Solution:
         n = len(nums)
         
                 
-        rightmax = [float('-inf')] * n
-        suffixsum = rightmax[-1] = nums[-1]
-        for i in range(n-2,-1,-1):
-            suffixsum += nums[i]
-            rightmax[i] = max(suffixsum,rightmax[i+1])
         
         
         maxsum = float('-inf')
-        specialsum = float('-inf')
         cursum = 0
-        presum = 0
         for i,val in enumerate(nums):
             cursum += val
-            presum += nums[i]
             if cursum > maxsum:
                 maxsum = cursum
             if cursum < 0:
                 cursum = 0
-            if i+1 < n:
-                specialsum = max(specialsum,presum+rightmax[i+1])
                 
-        return max(maxsum,specialsum)
+        minsum = float('inf')
+        cursum = 0
+        for i,val in enumerate(nums):
+            cursum += val
+            if cursum < minsum:
+                minsum = cursum
+            if cursum > 0:
+                cursum = 0
+        
+        total = sum(nums)
+        # print(total,minsum)
+        if total == minsum:
+            return maxsum
+        return max(maxsum,total-minsum)
             
                 
