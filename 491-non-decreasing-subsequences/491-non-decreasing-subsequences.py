23737a6a8deaa17ad706844c245fef59788b322d
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-01-20 16:04:20 +0300

    Time: 251 ms (59.84%), Space: 22.5 MB (28.91%) - LeetHub

--- 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
+++ 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
@@ -3,15 +3,18 @@ class Solution:
         ans = set()
         def findsubseq(index,subseq):
             if len(subseq) > 1:
-                items = tuple(subseq)
-                ans.add(items)
-                # ans.append(subseq)
+                ans.add(tuple(subseq))
             for i in range(index,len(nums)):
                 if nums[i] >= subseq[-1]:
                     copy = subseq.copy()
                     copy.append(nums[i])
                     findsubseq(i+1,copy)
-            if index < len(nums) : findsubseq(index+1,[nums[index]])
+            if index < len(nums) and start[index]:
+                findsubseq(index+1,[nums[index]])
+                start[index] = 0
+                
+        start = [1] * len(nums)
+        start[0] = 0
         findsubseq(1,[nums[0]])
         return ans
         
