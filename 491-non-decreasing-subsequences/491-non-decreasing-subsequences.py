86d6d6f6d902358ac0e3038cfa9108e8019c46be
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-01-20 17:22:38 +0300

    Time: 251 ms (59.84%), Space: 22.6 MB (28.91%) - LeetHub

--- 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
+++ 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
@@ -1,15 +1,5 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        result = set()
-        for bitmask in range(1, 1 << n):
-            # build the sequence
-            sequence = [nums[i] for i in range(n) if (bitmask >> i) & 1]
-            # check if its length is at least 2, and it is increasing
-            if len(sequence) >= 2 and all([sequence[i] <= sequence[i + 1]
-                                          for i in range(len(sequence) - 1)]):
-                result.add(tuple(sequence))
-        return result
         ans = set()
         def findsubseq(index,subseq):
             if len(subseq) > 1:
@@ -28,3 +18,14 @@ class Solution:
         findsubseq(1,[nums[0]])
         return ans
         
+        
+        n = len(nums)
+        result = set()
+        for bitmask in range(1, 1 << n):
+            # build the sequence
+            sequence = [nums[i] for i in range(n) if (bitmask >> i) & 1]
+            # check if its length is at least 2, and it is increasing
+            if len(sequence) >= 2 and all([sequence[i] <= sequence[i + 1]
+                                          for i in range(len(sequence) - 1)]):
+                result.add(tuple(sequence))
+        return result
