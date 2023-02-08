fa0e63342937ae5c6b26afd4f510b4567ac3140e
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-02-08 11:59:28 +0300

    Time: 1184 ms (67.05%), Space: 68.4 MB (44.70%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -7,12 +7,10 @@ class Solution:
             neghbours[u].append(v)
             neghbours[v].append(u)
             
-        visited = set()
         queue = deque()
         for k,v in neghbours.items():
             if len(v) ==  1:
                 queue.append(k)
-                visited.add(k)
                 break
         
         ans = []
@@ -20,9 +18,9 @@ class Solution:
             val = queue.popleft()
             ans.append(val)
             for neghbour in neghbours[val]:
-                if neghbour not in visited:
+                if neghbours[neghbour]:
                     queue.append(neghbour)
-                    visited.add(neghbour)
+            neghbours.pop(val)
         return ans
             
             
