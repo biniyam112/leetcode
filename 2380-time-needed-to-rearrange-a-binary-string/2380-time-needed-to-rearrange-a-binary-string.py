badbd552c9fb35b6185e93ceca56468f8694033c
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-02-22 17:46:42 +0300

    Time: 2243 ms (26.98%), Space: 13.8 MB (63.89%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
@@ -3,11 +3,13 @@ class Solution:
         s = [int(x) for x in s]
         days = 0
         while True:
+            changes = 0
             temp = s.copy()
             for i in range(1,len(s)):
                 if s[i] and not s[i-1]:
+                    changes += 1
                     temp[i-1],temp[i] = temp[i],temp[i-1]
-            if temp == s:
+            if not changes:
                 return days
             else:
                 s = temp
