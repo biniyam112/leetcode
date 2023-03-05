8660535bca93c48d55ca34e42f333ae8cea941b7
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-03-05 03:42:39 -0500

    Time: 36 ms (41.81%), Space: 13.8 MB (55.73%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -4,13 +4,14 @@ class Solution:
             return x
         if n == 0:
             return 1
-        
         power = self.myPow(x,abs(n)//2)
-        if n < 0:
-            return 1/(power*power * self.myPow(x,n%2))
+        power *= power
+        
         if n % 2:
-            return power * power * x
-        else:
-            return power * power
+            power *= x
+        
+        if n < 0:
+            return 1/power
+        return power
         
         
