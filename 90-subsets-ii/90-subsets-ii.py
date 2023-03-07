7e4df3079f4373e0d1bd8c524db1354f2eba5141
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-03-07 08:18:30 -0500

    Time: 41 ms (55.84%), Space: 14.2 MB (40.16%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        ans.add(())
+        def makeSuperset(index):
+            if index == len(nums):
+                return
+            temp = set()
+            for item in ans:
+                added = item+tuple([nums[index]])
+                temp.add(tuple(sorted(added)))
+            ans.add(tuple([nums[index]]))
+            ans.update(temp)
+            makeSuperset(index+1)
+        makeSuperset(0)
+        return ans
