bbf76058d461137d035c42353ed3cb1f47946986
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-03-09 03:08:19 -0500

    Time: 87 ms (92.48%), Space: 14 MB (27.02%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,5 +1,55 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+	
+        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
+            res = []
+            
+            # If we have run out of numbers to add, return res.
+            if not nums:
+                return res
+            
+            # There are k remaining values to add to the sum. The 
+            # average of these values is at least target // k.
+            average_value = target // k
+            
+            # We cannot obtain a sum of target if the smallest value
+            # in nums is greater than target // k or if the largest 
+            # value in nums is smaller than target // k.
+            if average_value < nums[0] or nums[-1] < average_value:
+                return res
+            
+            if k == 2:
+                return twoSum(nums, target)
+    
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for subset in kSum(nums[i + 1:], target - nums[i], k - 1):
+                        res.append([nums[i]] + subset)
+    
+            return res
+
+        def twoSum(nums: List[int], target: int) -> List[List[int]]:
+            res = []
+            s = set()
+    
+            for i in range(len(nums)):
+                if len(res) == 0 or res[-1][1] != nums[i]:
+                    if target - nums[i] in s:
+                        res.append([target - nums[i], nums[i]])
+                s.add(nums[i])
+    
+            return res
+
+        nums.sort()
+        return kSum(nums, target, 4)
+
+
+
+
+
+
+class Solution1:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         def diffExist(indices,indexSet):
             if len(indexSet) > 3 : return True
             for index in indexSet:
