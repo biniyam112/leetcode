9e3fa93d42f44fa8312e2ce8f53f79ebec3de24d
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2023-03-15 04:10:19 -0400

    Time: 1479 ms (15.89%), Space: 13.9 MB (63.55%) - LeetHub

---
+++ 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maximumBobPoints(self, numArrows: int, aliceArrows: List[int]) -> List[int]:
+        n = 12
+        def getSum(aliceArrows):
+            score = 0
+            for i in range(n):
+                if aliceArrows[i] < 0:
+                    score += i
+            return score
+        
+        def recursion(index,remArrow):
+            if index == n:
+                res = [x for x in aliceArrows]
+                res[0] -= remArrow
+                return res
+            take,notake = [0] * n,[0] * n
+            if remArrow > aliceArrows[index]:
+                take = recursion(index+1,remArrow-aliceArrows[index]-1).copy()
+                take[index] = -1
+            notake = recursion(index+1,remArrow).copy()
+            if getSum(take) > getSum(notake):
+                return take
+            return notake
+        
+        res = recursion(0,numArrows)
+        bobArrows = [0] * n
+        for i in range(1,n):
+            if res[i] < 0:
+                bobArrows[i] = aliceArrows[i]+abs(res[i])
+        bobArrows[0] += aliceArrows[0]-res[0]
+        return bobArrows
+            
+            
+        
