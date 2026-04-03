d1c1931c4b2b9fccb0a48a1e499230d94005e179
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2026-04-03 10:41:08 -0400

    Time: 93 ms (77.76%) | Memory: 36.4 MB (78.51%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -1,12 +1,10 @@
 class Solution:
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
-        order = list(range(len(positions)))
-
-        zipped = sorted(zip(positions,healths,directions,order))
-        positions,healths,directions,order = [list(x) for x in zip(*zipped)]
+        n = len(positions)
+        order = sorted(range(n), key=lambda i: positions[i])
 
         monostack = []
-        for i in range(len(directions)):
+        for i in order:
             # Going to Left
             if directions[i] == 'L':
                 while monostack: # There are robots going to Right in stack
@@ -28,9 +26,7 @@ class Solution:
             else:
                 # Going to Right
                 monostack.append(i)
-        
-        zipped = sorted(zip(order,healths))
-        _,healths = [list(x) for x in zip(*zipped)]
+
         healths = [x for x in healths if x != 0]
         return healths
         
